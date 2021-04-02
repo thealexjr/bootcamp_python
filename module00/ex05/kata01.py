64e071cb8ed51e27ae83fd5ff3d811fecb8ce6bf
Meriem Elkhal <65033615+sprixie@users.noreply.github.com>
2021-04-02 19:49:34 +0200

    added kata01.py

---
+++ module00/ex05/kata01.py
@@ -0,0 +1,7 @@
+languages = {
+'Python': 'Guido van Rossum',
+'Ruby': 'Yukihiro Matsumoto',
+'PHP': 'Rasmus Lerdorf',
+}
+for item in languages:
+                print(item + " was created by " + languages[item])
