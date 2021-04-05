b74d4f5a429bd417fa40e56ae5a128fd3db799b0
thealexjr <80365738+thealexjr@users.noreply.github.com>
2021-04-05 22:48:07 +0200

    Update whois.py

--- module00/ex02/whois.py
+++ module00/ex02/whois.py
@@ -1,15 +1,22 @@
 import sys
 
-arg = sys.argv
-if arg[1].isnumeric():
-        if len(arg)==2:
-                if int(arg[1])==0:
-                        print("I'm Zero")
-                elif int(arg[1])%2==0:
-                        print("I'm even")
-                else:
-                        print("I'm odd")
-        else:
-                print('ERROR')
-else:
+
+def oddchecker(a):
+    n = int(a)
+    if n == 0:
+        print("I'm zero.")
+    elif n % 2 == 0:
+        print("I'm even.")
+    elif n % 2 != 0:
+        print("I'm odd.")
+    else:
         print("ERROR")
+
+
+if len(sys.argv[1:]) == 1:
+    if sys.argv[1].isdigit():
+        oddchecker(sys.argv[1])
+    else:
+        print("ERROR")
+else:
+    print("ERROR")
